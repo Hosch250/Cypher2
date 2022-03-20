f8f1c3236e0bbe628009bfef8446aeda2817774b
Abraham Hosch <hosch250@live.com>
2022-03-19 20:54:29 -0500

    Add project files.

---
+++ ClassLibrary1/CypherGenerator.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    [Generator]
+    public class HelloSourceGenerator : ISourceGenerator
+    {
+        private const string CypherAttribute = @"// Auto-generated code
+namespace CypherGenerator;
+
+public class CypherAttribute : System.Attribute
+{
+    public string Statement { get; }
+    public CypherAttribute(string statement)
+    {
+        Statement = statement;
+    }
+}";
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var types = GetCypherMethods(context.Compilation);
+            foreach (var declaration in types.GroupBy(g => g.ContainingType, SymbolEqualityComparer.Default))
+            {
+                var builder = new StringBuilder();
+
+                var methods = declaration.Select(symbol =>
+                {
+                    var querySyntaxNode = symbol.GetAttributes().First(f => f.AttributeClass.Name == "Cypher" || f.AttributeClass.Name == "CypherAttribute").ApplicationSyntaxReference.GetSyntax();
+                    var query = (querySyntaxNode as AttributeSyntax).ArgumentList.Arguments[0].Expression.ToFullString();
+
+                    var methodReturnType = (symbol.ReturnType as INamedTypeSymbol).TypeArguments[0];
+
+                    var resultMethod = methodReturnType.AllInterfaces.Any(i => i.Name == "IEnumerable") || methodReturnType.Name == "IEnumerable"
+                        ? "ToListAsync"
+                        : "SingleAsync";
+
+                    var resultType = methodReturnType.AllInterfaces.Any(i => i.Name == "IEnumerable") || methodReturnType.Name == "IEnumerable"
+                        ? (methodReturnType as INamedTypeSymbol).TypeArguments[0] as INamedTypeSymbol
+                        : methodReturnType as INamedTypeSymbol;
+
+                    var resultCtor = resultType.Constructors.First(f => f.Parameters.Length > 0 && !f.Parameters[0].Type.Equals(resultType, SymbolEqualityComparer.IncludeNullability));
+                    var ctorArgs = resultCtor.Parameters.Select(s => $@"f[""{s.Name[0].ToString().ToLower()}{s.Name.Substring(1)}""].As<{s.Type.ToDisplayString()}>()");
+
+                    return
+$@"    public partial async {symbol.ReturnType.ToDisplayString()} {symbol.Name}()
+    {{
+        using var session = driver.AsyncSession();
+
+        var query = new Neo4j.Driver.Query({query});
+        var result = await session.RunAsync(query);
+
+        return await result.{resultMethod}(f => new {resultType.ToDisplayString()}({string.Join(", ", ctorArgs)}));
+    }}";
+        });
+
+                var typeSymbol = declaration.Key;
+                builder.Append(
+$@"// Auto-generated code
+namespace {typeSymbol.ContainingNamespace.ToDisplayString()};
+using Neo4j.Driver;
+
+public partial class {typeSymbol.Name}
+{{
+    private IDriver driver;
+
+    public {typeSymbol.Name}(IDriver driver)
+    {{
+        this.driver = driver;
+    }}
+
+{string.Join($"{Environment.NewLine}{Environment.NewLine}", methods)}
+}}");
+
+                context.AddSource($"{typeSymbol.Name}.g.cs", builder.ToString());
+            }
+
+            // Add the source code to the compilation
+            context.AddSource("CypherAttribute.g.cs", CypherAttribute);
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // No initialization required for this one
+#if DEBUG
+            if (!Debugger.IsAttached)
+            {
+                //Debugger.Launch();
+            }
+#endif 
+        }
+
+        private static ImmutableArray<IMethodSymbol> GetCypherMethods(Compilation compilation)
+        {
+            IEnumerable<SyntaxNode> allNodes = compilation.SyntaxTrees.SelectMany(s => s.GetRoot().DescendantNodes());
+            IEnumerable<MethodDeclarationSyntax> allMethods = allNodes
+                .Where(d => d.IsKind(SyntaxKind.MethodDeclaration))
+                .OfType<MethodDeclarationSyntax>();
+
+            return allMethods
+                .Where(component => component.AttributeLists
+                    .SelectMany(s => s.Attributes)
+                    .Any(w => w.Name.ToString() == "Cypher" || w.Name.ToString() == "CypherAttribute"))
+                .Select(s => compilation.GetSemanticModel(s.SyntaxTree).GetDeclaredSymbol(s))
+                .ToImmutableArray();
+        }
+    }
+}
---
+++ Cypher2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Cypher2
+{
+    using Neo4j.Driver;
+    using CypherGenerator;
+
+    public partial class Program
+    {
+        static void Main()
+        {
+        }
+        
+        [Cypher("match (n:Person) return n.name as name, n.born as born")]
+        public partial Task<IEnumerable<Person>> GetPersons();
+
+        [Cypher("match (n:Person { name: \"Tom Hanks\"}) return n.name as name, n.born as born")]
+        public partial Task<Person> GetPerson();
+
+        [Cypher("match (n:Movie) return n.title as title, n.tagline as tagline, n.released as released")]
+        public partial Task<IEnumerable<Movie>> GetMovies();
+    }
+
+    public record Person(string Name, long? Born);
+    public record Movie(string Title, string Tagline, int Released);
+}
